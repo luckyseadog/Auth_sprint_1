130de01aeb4c1d47fcfac6c18046bf414c403bd1
Aleksey Skovorodnikov <alexskovorodnikov@yandex.ru>
2024-06-28 12:58:04 +0300

    refactor services user

--- async_api/auth_service/src/services/exceptions.py
+++ async_api/auth_service/src/services/exceptions.py
@@ -16,21 +16,42 @@ def user_not_found(user_id: str) -> HTTPException:
 
 
 def role_already_exists(role_id: str) -> HTTPException:
-    raise HTTPException(
+    return HTTPException(
         status_code=status.HTTP_409_CONFLICT,
         detail=f'role {role_id} already exists',
     )
 
 
 def role_not_exists(role_id: str) -> HTTPException:
-    raise HTTPException(
+    return HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail=f'role {role_id} not exists in user',
     )
 
 
 def invalid_user_name_or_password() -> HTTPException:
-    raise HTTPException(
+    return HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail='Invalid username or password',
+    )
+
+
+def email_already_exists(email: str) -> HTTPException:
+    return HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f'email {email} already exists',
+    )
+
+
+def login_already_exists(login: str) -> HTTPException:
+    return HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f'email {login} already exists',
+    )
+
+
+def user_already_deleted(user_id: str) -> HTTPException:
+    return HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f'User with id {user_id} already deleted',
     )
--- async_api/auth_service/src/services/user_service.py
+++ async_api/auth_service/src/services/user_service.py
@@ -9,7 +9,13 @@ from schemas.entity import Role, User
 from services.password_service import password_service
 from functools import lru_cache
 from db.postgres_db import get_session
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
+from services.exceptions import (
+    user_not_found,
+    email_already_exists,
+    login_already_exists,
+    user_already_deleted,
+)
 
 
 class UserService:
@@ -20,26 +26,8 @@ class UserService:
         stmt = await self.db.execute(select(UserModel).where(UserModel.id == user_id))
         user = stmt.scalars().one_or_none()
         if not user:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f'not found user: {user_id}',
-            )
-        return User(
-            id=user.id,
-            login=user.login,
-            password=user.password,
-            first_name=user.first_name,
-            last_name=user.last_name,
-            email=user.email,
-            is_superadmin=user.is_superadmin,
-            roles=[
-                Role(
-                    id=role.id,
-                    title=role.title,
-                    description=role.description,
-                ) for role in user.roles
-            ],
-        )
+            raise user_not_found(user_id)
+        return user
 
     async def get_user_by_email(self, user_email: str) -> User | None:
         stmt = await self.db.execute(select(UserModel).where(UserModel.email == user_email))
@@ -85,33 +73,16 @@ class UserService:
 
     async def get_users(self, skip: int = 0, limit: int = 100) -> list[User]:
         result = await self.db.execute(select(UserModel).offset(skip).limit(limit))
-        return [
-            User(
-                id=user.id,
-                login=user.login,
-                password=user.password,
-                first_name=user.first_name,
-                last_name=user.last_name,
-                email=user.email,
-                is_superadmin=user.is_superadmin,
-                roles=[
-                    Role(
-                        id=role.id,
-                        title=role.title,
-                        description=role.description,
-                    ) for role in user.roles
-                ],
-            ) for user in result.scalars()
-        ]
+        return result.scalars().all()
 
     async def create_user(self, user_create: User) -> User:
         user = await self.get_user_by_email(user_create.email)
         if user:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='email already exists')
+            raise email_already_exists(user_create.email)
 
         user = await self.get_user_by_login(user_create.login)
         if user:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='login already exists')
+            raise login_already_exists(user_create.login)
 
         user_create.password = password_service.compute_hash(user_create.password)
         user_dto = jsonable_encoder(user_create, exclude_none=True)
@@ -131,45 +102,29 @@ class UserService:
         self.db.add(user)
         await self.db.commit()
         await self.db.refresh(user)
-        return User(
-            id=user.id,
-            login=user.login,
-            password=user.password,
-            first_name=user.first_name,
-            last_name=user.last_name,
-            email=user.email,
-            is_superadmin=user.is_superadmin,
-            roles=[
-                # jsonable_encoder(
-                Role(
-                    id=role.id,
-                    title=role.title,
-                    description=role.description,
-                ) for role in user.roles
-                #        )
-            ],
-        )
+        return user_create
 
     async def update_user(self, user_patch: User):
         user_patch.password = password_service.compute_hash(user_patch.password)
 
         user = await self.get_user_by_email(user_patch.email)
         if user and user.id != user_patch.id:
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f'User with this email {user.email} already exists',
-            )
+            raise email_already_exists(user_patch.email)
         user = await self.get_user_by_login(user_patch.login)
         if user and user.id != user_patch.id:
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f'User with this login {user.login} already exists',
-            )
+            raise login_already_exists(user_patch.login)
 
         query = (
             update(UserModel)
             .where(UserModel.id == user_patch.id)
-            .values(**user_patch.model_dump(exclude_none=True))
+            # .values(**user_patch.model_dump(exclude_none=True))
+            .values({
+                UserModel.login: user_patch.login,
+                UserModel.password: user_patch.password,
+                UserModel.first_name: user_patch.first_name,
+                UserModel.last_name: user_patch.last_name,
+                UserModel.email: user_patch.email,
+            })
             .returning(UserModel)
         )
         result = await self.db.execute(query)
@@ -177,35 +132,30 @@ class UserService:
         await self.db.commit()
 
         if not updated_user:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f'User with id {user_patch.id} not found',
-            )
+            raise user_not_found(user_patch.id)
 
-        return User(
-            id=updated_user.id,
-            login=updated_user.login,
-            password=updated_user.password,
-            first_name=updated_user.first_name,
-            last_name=updated_user.last_name,
-            email=updated_user.email,
-            is_superadmin=updated_user.is_superadmin,
-            roles=[
-                Role(
-                    id=role.id,
-                    title=role.title,
-                    description=role.description,
-                ) for role in updated_user.roles
-            ],
-        )
+        return updated_user
+
+        # return User(
+        #     id=updated_user.id,
+        #     login=updated_user.login,
+        #     password=updated_user.password,
+        #     first_name=updated_user.first_name,
+        #     last_name=updated_user.last_name,
+        #     email=updated_user.email,
+        #     is_superadmin=updated_user.is_superadmin,
+        #     roles=[
+        #         Role(
+        #             id=role.id,
+        #             title=role.title,
+        #             description=role.description,
+        #         ) for role in updated_user.roles
+        #     ],
+        # )
 
     async def delete_user(self, user_id: str) -> User:
-
         if not self.is_deleted(user_id):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f'User with id {user_id} already deleted',
-            )
+            raise user_already_deleted(user_id)
 
         query = (
             update(UserModel)
@@ -218,34 +168,13 @@ class UserService:
         deleted_user = result.scalars().one_or_none()
         await self.db.commit()
         if not deleted_user:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f'User with id {user_id} not found',
-            )
-        return User(
-            id=deleted_user.id,
-            login=deleted_user.login,
-            password=deleted_user.password,
-            first_name=deleted_user.first_name,
-            last_name=deleted_user.last_name,
-            email=deleted_user.email,
-            is_superadmin=deleted_user.is_superadmin,
-            roles=[
-                Role(
-                    id=role.id,
-                    title=role.title,
-                    description=role.description,
-                ) for role in deleted_user.roles
-            ],
-        )
+            raise user_not_found(user_id)
+        return deleted_user
 
     async def is_deleted(self, user_id: str):
         user = await self.get_user(user_id)
         if not user:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f'User with id {user_id} not found',
-            )
+            raise user_not_found(user_id)
         return False if user.deleted_at is None else True
 
 
@@ -253,5 +182,4 @@ class UserService:
 def get_user_service(
         db: AsyncSession = Depends(get_session),
 ) -> UserService:
-    # redis: RedisTokenStorage = Depends(get_redis)) -> UserService:
     return UserService(db)
